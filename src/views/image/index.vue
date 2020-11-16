3574ecd2c6eebcdd8ec4da9a47a27f5859e39295
Corrine019 <517888558@qq.com>
2020-11-16 16:58:33 +0800

    素材发布页代码修改

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -11,7 +11,7 @@
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
       <div class="action-head">
-        <el-radio-group v-model="collect" size="mini" @change="onCollectChange">
+        <el-radio-group v-model="collect" size="mini" @change="loadImages(1)">
           <el-radio-button :label="false">全部</el-radio-button>
           <el-radio-button :label="true">收藏</el-radio-button>
         </el-radio-group>
@@ -19,15 +19,62 @@
       </div>
       <!-- 素材列表 -->
       <el-row :gutter="10">
-        <el-col v-for="(img, index) in images" :key="index" :xs="12" :sm="6" :md="6" :lg="4">
+        <el-col
+          :xs="12"
+          :sm="6"
+          :md="6"
+          :lg="4"
+          v-for="(img, index) in images"
+          :key="index"
+          class="image-item"
+          @click.native="selected = index"
+        >
           <el-image
-            style="width: 100px; height: 100px"
-            src="img.url"
-            fit="cover">
-          </el-image>
+            style="height: 100px"
+            :src="img.url"
+            fit="cover"
+          ></el-image>
+          <div
+            class="selected"
+            v-if="isShowSelected && selected === index"
+          ></div>
+          <div v-if="isShowAction" class="image-action">
+            <!--
+              class 样式绑定
+               {
+                  CSS类名: 布尔值
+               }
+               true：作用类名
+               false：不作用类名
+             -->
+            <el-button
+              type="warning"
+              :icon="img.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
+              circle
+              size="small"
+              @click="onCollect(img)"
+              :loading="img.loading"
+            ></el-button>
+            <el-button
+              size="small"
+              type="danger"
+              icon="el-icon-delete-solid"
+              circle
+              :loading="img.loading"
+              @click="onDelete(img)"
+            ></el-button>
+            </div>
         </el-col>
       </el-row>
       <!-- /素材列表 -->
+      <!-- 数据分页 -->
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        @current-change="onPageChange"
+        :total="totalCount">
+      </el-pagination>
+      <!-- /数据分页 -->
     </el-card>
 
     <el-dialog :append-to-body="true" title="上传素材" :visible.sync="dialogUploadVisible">
@@ -58,32 +105,53 @@ export default {
   data () {
     const user = JSON.parse(window.localStorage.getItem('user'))
     return {
-      collect: false,
-      images: [],
+      collect: false, // 默认查询全部素材列表
+      images: [], // 图片素材列表
       dialogUploadVisible: false,
       uploadHeaders: {
         Authorization: `Bearer ${user.token}`
-      }
+      },
+      totalCount: 0, // 总数据条数
+      pageSize: 20, // 每页大小
+      page: 1, // 当前页码
+      selected: null // 选中的索引
     }
   },
   computed: {},
   watch: {},
   created () {
-    this.loadImages(false)
+    this.loadImages(1)
   },
   mounted () {},
   methods: {
-    loadImages (collect = false) {
-      getImages({ collect: true }).then(res => {
-        this.images = res.data.data.results
+    loadImages (page = 1) {
+      // 重置高亮页码，防止数据和页码不对应
+      this.page = page
+      getImages({
+        collect: this.collect,
+        page,
+        per_page: this.pageSize
+      }).then(res => {
+        const results = res.data.data.results
+        results.forEach(img => {
+          // img 对象本来没有 loading 数据
+          // 我们这里收到的往里面添加该数据是用来控制每个收藏按钮的 loading 状态
+          img.loading = false
+        })
+        this.images = results
+        this.totalCount = res.data.data.total_count
       })
-    },
-    onCollectChange (value) {
-      this.loadImages(value)
     },
     onUploadSuccess () {
       this.dialogUploadVisible = false
-      this.loadImages(false)
+      this.loadImages(this.page)
+      this.$message({
+        type: 'success',
+        message: '上传成功'
+      })
+    },
+    onPageChange (page) {
+      this.loadImages(page)
     }
   }
 }
