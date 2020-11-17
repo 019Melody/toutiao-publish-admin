b938d1085419acb10be8b1c74ca16b4e1ed5f4ff
Corrine019 <517888558@qq.com>
2020-11-17 11:25:25 +0800

    评论管理完成138

--- src/api/article.js
+++ src/api/article.js
@@ -79,3 +79,17 @@ export const deleteImage = imageId => {
     url: `/mp/v1_0/user/images/${imageId}`
   })
 }
+
+// 修改文章评论状态
+export const updateCommentStatus = (articleId, allowComment) => {
+  return request({
+    method: 'PUT',
+    url: '/mp/v1_0/comments/status',
+    params: {
+      article_id: articleId
+    },
+    data: {
+      allow_comment: allowComment
+    }
+  })
+}
